3b3254f5f21a0d35f005a0e8a7dc05d6aa583bd4
cangokceaslan <cangokceaslanx@gmail.com>
2020-12-06 16:37:12 +0300

    Real Time Chatting
    Real Time Chatting

---
+++ index.js
@@ -0,0 +1,36 @@
+var app = require("express")();
+var express = require("express");
+var http = require('http').Server(app);
+var io = require("socket.io")(http);
+var path = '';
+var md5 = "";
+var css = __dirname+'/css/styles.css';
+app.get(path, function(req, res){
+    path = __dirname+'/index.html';
+  res.sendFile(path);
+});
+app.get("/css/styles.css", function(req, res){
+  res.sendFile(css);
+});
+app.get("/message.html", function(req, res){
+  res.sendFile(__dirname+"/message.html");
+  md5 = req.query.md5;
+});
+app.get('/notification.mp3', function(req, res){
+  res.sendFile(__dirname+'/notification.mp3');
+});
+app.get(css, function(req, res){
+  res.sendFile( css);
+});
+http.listen((process.env.PORT || 80), function(){
+  console.log('listening on *:80');
+});
+io.on('connection', function(socket){
+  socket.on('disconnect',function(){
+    console.log("A user disconnected");
+  });
+  socket.on(md5, function(msg){
+    io.emit(md5, msg);
+    console.log(msg);
+   });
+});
